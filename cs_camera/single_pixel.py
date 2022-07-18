bda6fe6f16f60b7e0cd965bc5cc45b9a79501eb2
yaroslavorl <jaroslavorlenko@yandex.ru>
2022-07-18 04:30:21 +0300

    fista cs camera

--- cs_camera/single_pixel.py
+++ cs_camera/single_pixel.py
@@ -28,10 +28,10 @@ def make_dct_matrix(n):
 
 def make_patterns(img_size, num_patterns):
 
-    patterns = np.random.rand(num_patterns, img_size, img_size)
-    patterns = np.where(patterns > 0.1, 0, 1)
+    patterns = np.random.rand(num_patterns, img_size ** 2)
+    patterns = np.where(patterns > 0.5, 1, -1)
 
-    return patterns.reshape(num_patterns, img_size ** 2)
+    return patterns
 
 
 def make_sparse_mask(img_size, percent):
@@ -40,39 +40,57 @@ def make_sparse_mask(img_size, percent):
     return np.where(mask > percent, 0, 1)
 
 
+def soft_tresh(img, param):
+    return np.sign(img) * np.maximum(np.abs(img) - param, 0.)
+
+
 def least_squares(img):
     return np.sum(img ** 2)/2
 
 
 def loss(img, param):
-    return least_squares(P.dot(img) - m) + param*np.linalg.norm(img, 1)
+    return least_squares(P.dot(img) - m)/num_patterns + param*np.linalg.norm(img, 1)
 
 
 def gradient(img, param):
     return 2/num_patterns * P.T.dot(np.dot(P, img) - m) + param*np.sign(img)
 
 
-def gradient_descent(img, param=0.05, learning_rate=0.01, epochs=500):
+def gradient_descent(img, param=0.01, learning_rate=0.1, epochs=20):
+
+    t = 1
+    new_img = img.copy()
 
     for epoch in range(epochs):
-        img = img - learning_rate * gradient(img, param)
+
+        old_img = img.copy()
+        new_img = new_img - learning_rate * gradient(new_img, param)
+        img = soft_tresh(new_img, param / learning_rate)
+
+        t_0 = t
+        t = (1 + np.sqrt(1 + 4 * t ** 2)) / 2
+        new_img = img + ((t_0 - 1) / t) * (img - old_img)
 
         print('Эпоха: ', epoch, 'Ошибка: ', loss(img, param))
 
     return img
 
 
-num_patterns = 100
+num_patterns = 3000
 percent_pixel = 0.1
-img_orig = cv2.imread('test.jpeg', 0)
+
+img_orig = cv2.imread('test_img/lena_0.jpeg', 0)
 img_orig = img_orig.astype(np.float64) / 255
 
 size_2d_img = img_orig.shape
 size_img = img_orig.flatten().shape[0]
 
+print('Заполнение базиса')
 dct_matrix = make_dct_matrix(size_img)
+print('Базис заполнен')
 
 patterns = make_patterns(size_2d_img[0], num_patterns)
+print('Паттерны созданы')
 m = single_pixel_detector(img_orig.flatten(), patterns)
 img = make_sparse_mask(size_2d_img[0], percent_pixel) * img_orig
 
@@ -84,3 +102,4 @@ P = patterns.dot(idct_matrix)
 
 img = gradient_descent(img)
 cv2.imwrite('1.jpeg', dct_matrix.T.dot(img * 255).reshape(size_2d_img))
+
